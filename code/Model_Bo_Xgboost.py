5e000be290d67e60c083904ad7ff3e17ed3bee11
algo21-221040114 <221040114@link.cuhk.edu.cn>
2022-04-28 11:39:11 +0800

    edit

--- code/Model_Bo_Xgboost.py
+++ code/Model_Bo_Xgboost.py
@@ -1,28 +1,88 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import cross_val_score
+import pickle
 import xgboost as xgb
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
-import BayesianOptimization
+from matplotlib import pyplot as plt
+from bayes_opt import BayesianOptimization
+
+pre_length = 1
+factor_num = 5
+
+data_input = open('C:/Users/221040114/Desktop/code/code/train_x.pkl', 'rb')
+a = pickle.load(data_input)
+train_x = np.ones((800, pre_length*factor_num))
+for i in range(800):
+    for j in range(pre_length):
+        for k in range(factor_num):
+            train_x[i, j * pre_length + k] = a[i, j, k]
+# print(train_x.shape)
+
+data_input = open('C:/Users/221040114/Desktop/code/code/train_y.pkl', 'rb')
+train_y = pickle.load(data_input)
+train_data = xgb.DMatrix(train_x, train_y)
+
+data_input = open('C:/Users/221040114/Desktop/code/code/test_x.pkl', 'rb')
+a = pickle.load(data_input)
+data_input = open('C:/Users/221040114/Desktop/code/code/forecast_y.pkl', 'rb')
+b = pickle.load(data_input)
+test_x = np.ones((205, pre_length*factor_num+1))
+for i in range(205):
+    for j in range(pre_length):
+        for k in range(factor_num):
+            test_x[i, j * pre_length + k] = a[i, j, k]
+    test_x[i, -1] = b[i, 0]
+print(test_x)
+test_x = xgb.DMatrix(test_x)
+
+data_input = open('C:/Users/221040114/Desktop/code/code/test_y.pkl', 'rb')
+test_y = pickle.load(data_input)
+
+# # General XGBoost Model
+# params_stock = {
+#     'max_depth': 10,
+#     'learning_rate': 0.1,
+#     'min_child_weight': 3,
+#     'reg_alpha': 2,
+#     'colsample_bytree': 0.5,
+#     'seed': 731
+# }
+#
+# model_cv = xgb.cv(params_stock, dtrain=train_data, nfold=5, num_boost_round=100)
+# print(model_cv)
+# model_cv[['train-rmse-mean']].plot()
+# model_cv[['test-rmse-mean']].plot()
+# plt.show()
 
 
-data = pd.read_csv('/Users/wyb/PycharmProjects/Assignment3/database/AAPL.csv',
-                   index_col='Date')
-train_x = data.iloc[:800, :-1]
-train_y = data.iloc[:800, -1]
-model = xgb.Booster(params)
+# Build target function
+def model_cv(max_depth, learning_rate, min_child_weight, reg_alpha, colsample_bytree):
 
-# bo = BayesianOptimization(
-#     stock_xgb,
-#     {'n_estimators': (10, 15),
-#      'max_depth': (5, 10),
-#      'learning_rate': (0.01, 0.02),
-#      'min_child_weight': (1, 2),
-#      'reg_alpha': (1, 2),
-#      'reg_lamda': (1, 2),
-#      'colsample_bytree': (1, 2),
-#      'min_child_sample': (1, 2)
-#      }
-# )
-# bo.maximize(iteration=25)
+    params_stock = {
+        'max_depth': int(max_depth),
+        'learning_rate': learning_rate,
+        'min_child_weight': min_child_weight,
+        'reg_alpha': reg_alpha,
+        'colsample_bytree': colsample_bytree,
+        'seed': 0
+    }
+
+    model = xgb.cv(params=params_stock, dtrain=train_data, nfold=5)
+    val_score = model['test-rmse-mean'].iloc[-1]
+    return val_score*-1
+
+
+# Bayesian Optimization
+model_cv_bayes = BayesianOptimization(model_cv,
+                                      {'max_depth': (5, 10),
+                                       'learning_rate': (0.01, 0.1),
+                                       'min_child_weight': (2, 3),
+                                       'reg_alpha': (0.1, 0.2),
+                                       'colsample_bytree': (0.7, 1)
+                                       }
+                                      )
+model_cv_bayes.maximize(n_iter=25)
+dict_params = model_cv_bayes.max['params']
+dict_params['max_depth'] = int(dict_params['max_depth'])
+best_model = xgb.train(dict_params, train_data, num_boost_round=100)
+pred_y = best_model.predict(test_x)
+print(pred_y)
--- code/Model_LSTM.py
+++ code/Model_LSTM.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import pickle
 from keras.layers import LSTM, Dense, Dropout
 from keras import Sequential
 
@@ -7,25 +8,35 @@ from keras import Sequential
 data = pd.read_csv('/Users/wyb/PycharmProjects/Assignment3/database/AAPL.csv',
                    index_col='Date')
 
-pre_length = 3
+pre_length = 1
 factor_num = 5
 train_x = np.ones((800, pre_length, factor_num))
 for i in range(pre_length, 800+pre_length):
     x = np.ones((pre_length, factor_num))
     for j in range(factor_num):
-        x[:, j] = data.iloc[i-3:i, j]
+        x[:, j] = data.iloc[i-pre_length:i, j]
     train_x[i-pre_length, :, :] = x
 train_y = data.iloc[pre_length:800+pre_length, -1]
 train_y = np.array(train_y)
+data_output = open('/Users/wyb/PycharmProjects/Assignment3/database/train_x.pkl', 'wb')
+pickle.dump(train_x, data_output)
+data_output = open('/Users/wyb/PycharmProjects/Assignment3/database/train_y.pkl', 'wb')
+pickle.dump(train_y, data_output)
+data_output.close()
 
 test_x = np.ones((data.shape[0]-800-pre_length, pre_length, factor_num))
 for i in range(800+pre_length, data.shape[0]):
     x = np.ones((pre_length, factor_num))
     for j in range(factor_num):
-        x[:, j] = data.iloc[i-3:i, j]
+        x[:, j] = data.iloc[i-pre_length:i, j]
     test_x[i-800-pre_length, :, :] = x
 test_y = data.iloc[800+pre_length:data.shape[0], -1]
 test_y = np.array(test_y)
+data_output = open('/Users/wyb/PycharmProjects/Assignment3/database/test_x.pkl', 'wb')
+pickle.dump(test_x, data_output)
+data_output = open('/Users/wyb/PycharmProjects/Assignment3/database/test_y.pkl', 'wb')
+pickle.dump(test_y, data_output)
+data_output.close()
 
 model = Sequential()
 model.add(LSTM(units=128, return_sequences=True, input_shape=(train_x.shape[1], factor_num)))
@@ -37,5 +48,11 @@ model.compile(optimizer='sgd', loss='mse', metrics=['accuracy', 'mse', 'mae'])
 model.fit(train_x, train_y, epochs=1, batch_size=64)
 pred_y = model.predict(test_x)
 rmse = np.sqrt(np.mean(pred_y-test_y)**2)
-print(rmse)
+# print(rmse)
+
+data_output = open('/Users/wyb/PycharmProjects/Assignment3/database/forecast_y.pkl', 'wb')
+pickle.dump(pred_y, data_output)
+data_output.close()
+
+
 
